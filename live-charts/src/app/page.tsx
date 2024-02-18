b46e18946850e400bf298da742c3d1b0bd196a35
Jannik Lorenz <janniklorenz1994@gmail.com>
2024-02-18 13:49:52 +0100

    feat: adds live chart

--- live-charts/src/app/page.tsx
+++ live-charts/src/app/page.tsx
@@ -1,37 +1,54 @@
 "use client";
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend, registerables,
+    Chart as ChartJS, registerables,
 } from 'chart.js'
 import {Line} from "react-chartjs-2";
+import {useEffect, useRef} from "react";
 
 export default function Home() {
+    const chartRef = useRef(null)
     const countOfDataPoints = 100
-
   const data=  {
       type: "line",
       datasets: [{
           id: "Dataset1",
-          data: new Array(countOfDataPoints).fill(null).map((_, i) => ({ y: Math.floor(Math.random() * 100), x: i })),
+          data: new Array(countOfDataPoints).fill(null).map(() => ({ y: RandomPointFactory(), x: new Date().toISOString() })),
           label: "# Response in ms",
           borderWith: 1,
-          backgroundColor: "white",
-          borderColor: "white",
+          backgroundColor: "#699cd3",
+          fill: true
+
       }],
-      labels: new Array(countOfDataPoints).fill(null).map((_, i) => i.toString())
+      labels: new Array(countOfDataPoints).fill(null).map(() => new Date().toISOString())
   }
 
   ChartJS.register(...registerables)
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+            addRandomPoint(data)
+            // @ts-ignore
+            chartRef?.current?.update()
+        }, 1000)
+
+        return () => clearInterval(interval)
+    }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Line data={data} options={{}}/>
+      <Line ref={chartRef} data={data} options={{}}/>
     </main>
   );
+}
+
+function addRandomPoint(data: any) {
+    const currentLength = data.datasets[0].data.length
+    data.labels.shift()
+    data.labels.push(new Date().toISOString())
+    data.datasets[0].data.shift()
+    data.datasets[0].data.push({ y: RandomPointFactory(), x: currentLength + 1 })
+}
+
+function RandomPointFactory() {
+    return Math.floor(Math.random() * 100)
 }
